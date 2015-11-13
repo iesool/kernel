f3212a7d697b8c59f1e648d8ee54d1e81214aee1
Matthias Brugger <mbrugger@suse.com>
2015-11-13 21:20:30 +0100

    irq-gic-v3-its: Setup PCI requester ID.
    suse-commit: 15d36a766640b8c08486f3547baa0111ec048602

--- drivers/irqchip/irq-gic-v3-its.c
+++ drivers/irqchip/irq-gic-v3-its.c
@@ -1258,7 +1258,6 @@ static int its_alloc_device_irq(struct its_device *dev, irq_hw_number_t *hwirq)
 
 struct its_pci_alias {
 	struct pci_dev	*pdev;
-	u32		dev_id;
 	u32		count;
 };
 
@@ -1276,7 +1275,6 @@ static int its_get_pci_alias(struct pci_dev *pdev, u16 alias, void *data)
 {
 	struct its_pci_alias *dev_alias = data;
 
-	dev_alias->dev_id = alias;
 	if (pdev != dev_alias->pdev)
 		dev_alias->count += its_pci_msi_vec_count(dev_alias->pdev);
 
@@ -1290,6 +1288,7 @@ static int its_msi_prepare(struct irq_domain *domain, struct device *dev,
 	struct its_node *its;
 	struct its_device *its_dev;
 	struct its_pci_alias dev_alias;
+	u32 dev_id;
 
 	if (!dev_is_pci(dev))
 		return -EINVAL;
@@ -1299,20 +1298,21 @@ static int its_msi_prepare(struct irq_domain *domain, struct device *dev,
 	dev_alias.count = nvec;
 
 	pci_for_each_dma_alias(pdev, its_get_pci_alias, &dev_alias);
+	dev_id = pci_msi_domain_get_msi_rid(domain, pdev);
 	its = domain->parent->host_data;
 
-	its_dev = its_find_device(its, dev_alias.dev_id);
+	its_dev = its_find_device(its, dev_id);
 	if (its_dev) {
 		/*
 		 * We already have seen this ID, probably through
 		 * another alias (PCI bridge of some sort). No need to
 		 * create the device.
 		 */
-		dev_dbg(dev, "Reusing ITT for devID %x\n", dev_alias.dev_id);
+		dev_dbg(dev, "Reusing ITT for devID %x\n", dev_id);
 		goto out;
 	}
 
-	its_dev = its_create_device(its, dev_alias.dev_id, dev_alias.count);
+	its_dev = its_create_device(its, dev_id, dev_alias.count);
 	if (!its_dev)
 		return -ENOMEM;
 
